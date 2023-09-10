34b6fe435e13d3a1fe991bc6080573b56221d2fd
Vedanti.U <vedanti.vvu@gmail.com>
2023-09-10 22:56:10 +0530

    working get request

--- app.js
+++ app.js
@@ -2,23 +2,55 @@ const express = require('express')
 const {connectToDb, getDb} = require('./db')
 //init app and middleware
 const app = express()
-//db variable to store connection
-let db
-//db connection
-connectToDb((err)=>{
-    if(!err){
-        app.listen(3000,()=>{
-            console.log('app listening on port 3000')
-        })
-    }
-    db = getDb()
-})
 
-// app.listen(3000,()=>{
-//     console.log('app listening on port 3000')
-// })
+const {MongoClient,ObjectId} = require('mongodb')
+
+let db;
+MongoClient.connect('mongodb://127.0.0.1:27017/bookstore')
+.then((client)=>{
+    console.log("Client",client)
+    db = client.db()
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 //routes
 app.get('/books',(req,res)=>{
-    res.json({mssg:"welcome to api"})
+    //array to store book
+    let books = []
+    console.log("before query")
+    console.log(db)
+    //this is db.books
+    db.collection('books')
+    .find() 
+    .forEach(book =>{
+        console.log(book)
+        books.push(book)
+    })
+    .then(()=>{
+        console.log("trying to print")
+        res.status(200).json(books)
+    })
+    .catch(()=>{
+        res.status(500).json({error:'Could not fetch docu'})
+    })
 })
+
+app.get('/books/:id',(req,res)=>{
+    //req.params.id
+    if(ObjectId.isValid(req.params.id)){
+        db.collection('books')
+        .findOne({_id: new ObjectId(req.params.id)})
+        .then(doc =>{
+            res.status(200).json(doc)
+        })
+        .catch(err =>{
+            res.status(500).json({error:'Bye'})
+        })
+    }
+    else{
+        res.status(500).json({error:'not valid id'})
+    }
+})
+app.listen(3000)
--- db.js
+++ db.js
@@ -6,15 +6,8 @@ let dbConnection
 
 module.exports ={
     connectToDb:(cb)=>{
-        MongoClient.connect('mongodb://localhost://27017/bookstore')
-        .then((client)=>{
-            dbConnection = client.db()
-            return cb()
-        })
-        .catch((err)=>{
-            console.log(err)
-            return cb(err)
-        })
+       
     },
     getDb:()=>{dbConnection}
 }
+//mongodb://localhost:27017/
