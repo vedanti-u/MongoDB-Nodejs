0466af76f2c6fc7d8da92fe12d10878ed4ac0ed0
Vedanti.U <vedanti.vvu@gmail.com>
2023-09-10 16:25:07 +0530

    connection establishing

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const {connectToDb, getDb} = require('./db')
 //init app and middleware
 const app = express()
-
-
-app.listen(3000,()=>{
-    console.log('app listening on port 3000')
+//db variable to store connection
+let db
+//db connection
+connectToDb((err)=>{
+    if(!err){
+        app.listen(3000,()=>{
+            console.log('app listening on port 3000')
+        })
+    }
+    db = getDb()
 })
+
+// app.listen(3000,()=>{
+//     console.log('app listening on port 3000')
+// })
 
 //routes
 app.get('/books',(req,res)=>{
--- db.js
+++ db.js
@@ -2,8 +2,19 @@ const {MongoClient} = require('mongodb')
 //mongoclient helps to connect with mongo db
 //one func to connect to db
 //one func to retrieve/returnn the db connection once cconnected
+let dbConnection
 
 module.exports ={
-    connectToDb:()=>{},
-    getDb:()=>{}
+    connectToDb:(cb)=>{
+        MongoClient.connect('mongodb://localhost://27017/bookstore')
+        .then((client)=>{
+            dbConnection = client.db()
+            return cb()
+        })
+        .catch((err)=>{
+            console.log(err)
+            return cb(err)
+        })
+    },
+    getDb:()=>{dbConnection}
 }
