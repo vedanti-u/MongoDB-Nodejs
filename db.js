aa571143ae814ce667dd1ce658facd910efc4419
Vedanti.U <vedanti.vvu@gmail.com>
2023-09-10 16:07:54 +0530

    added mongoclient

---
+++ db.js
@@ -0,0 +1,9 @@
+const {MongoClient} = require('mongodb')
+//mongoclient helps to connect with mongo db
+//one func to connect to db
+//one func to retrieve/returnn the db connection once cconnected
+
+module.exports ={
+    connectToDb:()=>{},
+    getDb:()=>{}
+}
